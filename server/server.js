a166c5ebbb6a51ae55f72496309a8c25a0b8148f
RimonDipta <rimondipta@gmail.com>
2026-01-06 20:58:26 +0600

    Sever Fix to Connect Frontend

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ startScheduler();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
